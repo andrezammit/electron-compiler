4125cbd16d4df12fe0db79b1656818399758849c
Andre Zammit <mawzer@gmail.com>
2016-11-17 23:12:05 +0100

    Initial commit.

---
+++ main.js
@@ -0,0 +1,47 @@
+var packager = require("electron-packager");
+
+var options =
+    {
+        "dir": "../Recipe Manager/",
+        "arch": "x64",
+        "platform": "win32",
+        "app-copyright": "André Zammit",
+        "app-version": "1.0.3",
+        "icon": "../Recipe Manager/icons/icon.ico",
+        "name": "Recipe Manager",
+        "ignore": 
+            [
+                "/.data",
+                "/.gitignore",
+                "/.jshintrc",
+                "/.vscode",
+                "/.credentials",
+                "/icons",
+                "/typings",
+                "/jsconfig.json",
+            ],
+        "out": "./releases",
+        "overwrite": true,
+        "prune": true,
+        "version-string": 
+        {
+            "CompanyName": "André Zammit",
+            "FileDescription": "Recipe Manager application.",
+            "OriginalFilename": "Recipe Manager.exe",
+            "ProductName": "Recipe Manager",
+            "InternalName": "Recipe Manager"
+        }
+    };
+
+packager(options,
+    function(error, appPaths)
+    {
+        if (error !== null)
+        {
+            console.log("Packaging failed: ", error);
+            return;
+        }
+
+        console.log("Packaged successfully to: " + appPaths[0]);
+    });
+
